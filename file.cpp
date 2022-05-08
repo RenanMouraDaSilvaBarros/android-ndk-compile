5cd05fe10439af6d9555f0cc5f101612b9091377
Renan Moura <naneraruom@gmail.com>
2022-05-08 13:52:15 -0300

    refactor

---
+++ file.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "programa em C++\n";
+    return 0;
+}
--- nkd-compile.sh
+++ nkd-compile.sh
@@ -1,10 +1,19 @@
-#arm-linux-gnueabi-gcc-10 --static cpp.cpp -o compiled 
-#arm-linux-gnueabi-g++  test.cc -o test
 TARGET1=aarch64-linux-android
 TARGET2=armv7a-linux-androideabi
 TARGET3=i686-linux-android
 TARGET4=x86_64-linux-android
 
-NDK=/home/naner/Android/Sdk/ndk/24.0.8215888/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++
-TARGET=$TARGET2-android21 test.cc
+#file with c++ code
+FILE=file.cpp
+
+#ndk path
+NDK_PATH=/home/naner/Android/Sdk/ndk/24.0.8215888/
+
+#Android architecture
+TARGET=$TARGET2-android21 
+
+#compile file.cpp file
+$NDK_PATH/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++ -TARGET $FILE
+
+#send to android
 adb push a.out /data/local/tmp
